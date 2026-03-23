5fe69bdf6f66327181d85db2da96275c7f6c8094
Abhishek Singh <69066833+Abhishek-Singh2016@users.noreply.github.com>
2026-03-23 23:04:59 +0530

    Initial commit

---
+++ refresh-ecr.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -e # Exit on any error
+
+# Variables expected from environment: 
+# TARGET_ROLE_ARN, REGION, SECRET_NAME, TARGET_NAMESPACE
+TARGET_ROLE_ARN="arn:aws:iam::702175642104:role/ecr-k8s-image-pull"
+SECRET_NAME="ecr-pull-image-secret"
+REGION="us-east-1"
+#TARGET_ROLE_ARN: ${{ secrets.AWS_ROLE_ARN }}
+TARGET_NAMESPACE="default"
+
+echo "Assuming IAM Role: $TARGET_ROLE_ARN"
+env
+aws sts assume-role \
+  --role-arn "$TARGET_ROLE_ARN" \
+  --role-session-name "K8sRefreshSession"
+
+JSON_OUT=$(aws sts assume-role \
+  --role-arn "$TARGET_ROLE_ARN" \
+  --role-session-name "K8sRefreshSession")
+
+echo "$(which sed)"
+
+echo "$JSON_OUT"
+echo "$(echo $JSON_OUT | sed -n 's/.*"AccessKeyId": "\([^"]*\)".*/\1/p')"
+# Parse credentials using sed
+export AWS_ACCESS_KEY_ID=$(echo $JSON_OUT | sed -n 's/.*"AccessKeyId": "\([^"]*\)".*/\1/p')
+export AWS_SECRET_ACCESS_KEY=$(echo $JSON_OUT | sed -n 's/.*"SecretAccessKey": "\([^"]*\)".*/\1/p')
+export AWS_SESSION_TOKEN=$(echo $JSON_OUT | sed -n 's/.*"SessionToken": "\([^"]*\)".*/\1/p')
+
+echo "Fetching ECR Login Token for $REGION..."
+ECR_PASSWORD=$(aws ecr get-login-password --region "$REGION")
+
+echo "Updating Kubernetes Secret: $SECRET_NAME in $TARGET_NAMESPACE"
+kubectl create secret docker-registry "$SECRET_NAME" \
+  --docker-server="702175642104.dkr.ecr.${REGION}.amazonaws.com" \
+  --docker-username=AWS \
+  --docker-password="$ECR_PASSWORD" \
+  --namespace "$TARGET_NAMESPACE" \
+  --dry-run=client -o yaml | kubectl apply -f -
+
+echo "ECR credentials refreshed successfully at $(date)"
